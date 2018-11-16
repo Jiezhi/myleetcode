faf17cb68a48cf92bf63a8a93ad3b2c11383280e
Jiezhi <Jiezhi.G@gmail.com>
2018-11-16 13:27:47 +0800

    70. Climbing Stairs

---
+++ 70.climbing_stairs.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+"""
+https://leetcode.com/problems/climbing-stairs/description/
+Created on 2018-11-16
+
+@author: 'Jiezhi.G@gmail.com'
+
+Reference: 
+"""
+
+
+class Solution:
+    def climbStairs(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        last = 1
+        sum = 1
+        for i in range(n - 1):
+            tmp = sum
+            sum = last + sum
+            last = tmp
+        return sum
+
+
+if __name__ == '__main__':
+    assert Solution().climbStairs(1) == 1
+    assert Solution().climbStairs(2) == 2
+    assert Solution().climbStairs(3) == 3
