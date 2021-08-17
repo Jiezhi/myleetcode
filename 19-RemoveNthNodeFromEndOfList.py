80a8442fa09e0ca36a1503f5ff73e9fa1e74bcdf
Jiezhi <Jiezhi.G@gmail.com>
2021-08-17 11:31:31 +0800

    update 19

--- 19-RemoveNthNodeFromEndOfList.py
+++ 19-RemoveNthNodeFromEndOfList.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python
 """
 https://leetcode.com/problems/remove-nth-node-from-end-of-list/
+https://leetcode.com/explore/featured/card/top-interview-questions-easy/93/linked-list/603/
 Created on 2018-12-29
 
 @author: 'Jiezhi.G@gmail.com'
 
 Reference: 
 """
+from typing import Optional
 
 from list_node import *
 
@@ -37,8 +39,45 @@ class Solution:
             return []
         return l
 
+    def removeNthFromEnd2(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
+        """
+        Hint: Maintain two pointers and update one with a delay of n steps.
+
+        208 / 208 test cases passed.
+        Status: Accepted
+        Runtime: 24 ms
+        Memory Usage: 14.3 MB
+
+        :param self:
+        :param head:
+        :param n:
+        :return:
+        """
+        first_point = head
+        second_point = head
+        # let the first point go n farther!
+        for _ in range(n):
+            first_point = first_point.next
+        # first point reach the end, means we need to delete the first node
+        if not first_point:
+            return head.next
+        # go together, second point always behind the first point n steps
+        while first_point and first_point.next:
+            first_point = first_point.next
+            second_point = second_point.next
+        second_point.next = second_point.next.next
+        return head
+
 
 def test():
+    assert Solution().removeNthFromEnd2(buildListNode([1, 2, 3]), 2) == buildListNode([1, 3])
+    assert Solution().removeNthFromEnd2(buildListNode([1, 2, 3, 4, 5]), 2) == buildListNode([1, 2, 3, 5])
+    assert Solution().removeNthFromEnd2(buildListNode([1, 2, 3, 4, 5]), 5) == buildListNode([2, 3, 4, 5])
+    assert Solution().removeNthFromEnd2(buildListNode([1, 2, 3, 4, 5]), 1) == buildListNode([1, 2, 3, 4])
+    assert Solution().removeNthFromEnd2(buildListNode([1]), 1) is None
+    assert Solution().removeNthFromEnd2(buildListNode([1, 2]), 2) == buildListNode([2])
+    assert Solution().removeNthFromEnd2(buildListNode([1, 2]), 1) == buildListNode([1])
+
     assert Solution().removeNthFromEnd(buildListNode([1, 2, 3]), 2) == buildListNode([1, 3])
     assert Solution().removeNthFromEnd(buildListNode([1, 2, 3, 4, 5]), 2) == buildListNode([1, 2, 3, 5])
     assert Solution().removeNthFromEnd(buildListNode([1, 2, 3, 4, 5]), 5) == buildListNode([2, 3, 4, 5])
@@ -46,3 +85,7 @@ def test():
     assert Solution().removeNthFromEnd(buildListNode([1]), 1) == []
     assert Solution().removeNthFromEnd(buildListNode([1, 2]), 2) == buildListNode([2])
     assert Solution().removeNthFromEnd(buildListNode([1, 2]), 1) == buildListNode([1])
+
+
+if __name__ == '__main__':
+    test()
