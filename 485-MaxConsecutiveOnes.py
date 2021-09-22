38d9a71fc503c0fd51b852d6556a25a34866a12b
耿欣欣 <Jiezhi@users.noreply.github.com>
2021-09-22 10:37:41 +0800

    update 485

--- 485-MaxConsecutiveOnes.py
+++ 485-MaxConsecutiveOnes.py
@@ -5,6 +5,11 @@ Github: https://github.com/Jiezhi/myleetcode
 Created on 2019/9/3
 
 Leetcode: https://leetcode.com/problems/max-consecutive-ones/
+https://leetcode.com/explore/challenge/card/september-leetcoding-challenge-2021/638/week-3-september-15th-september-21st/3982/
+
+GITHUB: https://github.com/Jiezhi/myleetcode
+
+Difficulty: Easy
 
 """
 from typing import List
@@ -12,6 +17,14 @@ from typing import List
 
 class Solution:
     def findMaxConsecutiveOnes(self, nums: List[int]) -> int:
+        """
+        42 / 42 test cases passed.
+        Status: Accepted
+        Runtime: 340 ms
+        Memory Usage: 14.1 MB
+        :param nums:
+        :return:
+        """
         if not nums:
             return 0
         ret = 0
