3198a9dc4d2e20321648addabd7ee235d898534d
耿欣欣 <Jiezhi@users.noreply.github.com>
2021-10-05 16:24:26 +0800

    update 70

--- 70-ClimbingStairs.py
+++ 70-ClimbingStairs.py
@@ -2,16 +2,42 @@
 """
 https://leetcode.com/problems/climbing-stairs/description/
 Created on 2018-11-16
+Updated on 2021-10-05
 
 @author: 'Jiezhi.G@gmail.com'
 
 Reference: More solution can refer to https://leetcode.com/explore/learn/card/recursion-i/255/recursion-memoization/2377/
+
+Difficulty: Easy
 """
+from functools import lru_cache
 
 
 class Solution:
-    def climbStairs(self, n):
+    @lru_cache(None)
+    def climbStairs(self, n: int) -> int:
         """
+        Updated on 2021-10-05, Using DP
+        45 / 45 test cases passed.
+        Status: Accepted
+        Runtime: 55 ms
+        Memory Usage: 14.4 MB
+        :param n:
+        :return:
+        """
+        if n == 1:
+            return 1
+        if n == 2:
+            return 2
+        else:
+            return self.climbStairs(n - 1) + self.climbStairs(n - 2)
+
+    def climbStairs2(self, n):
+        """
+        45 / 45 test cases passed.
+        Status: Accepted
+        Runtime: 24 ms
+        Memory Usage: 13.9 MB
         :type n: int
         :rtype: int
         """
@@ -28,6 +54,8 @@ def test():
     assert Solution().climbStairs(1) == 1
     assert Solution().climbStairs(2) == 2
     assert Solution().climbStairs(3) == 3
+    assert Solution().climbStairs(45) == 1836311903
+    assert Solution().climbStairs2(45) == 1836311903
 
 
 if __name__ == '__main__':
