6ed4bdd1507aca35dc461a20d5d7112d07d99a59
Jiezhi <Jiezhi@users.noreply.github.com>
2021-11-10 16:24:50 +0800

    update 122

--- src/122-BestTimetoBuyandSellStockII.py
+++ src/122-BestTimetoBuyandSellStockII.py
@@ -1,17 +1,37 @@
 #!/usr/bin/env python
 """
 Created on 2020/12/2
+Updated on 2021/11/10
 
 Des:  https://leetcode.com/problems/best-time-to-buy-and-sell-stock-ii/
 
 https://leetcode.com/explore/featured/card/top-interview-questions-easy/92/array/564/
 
+Difficulty: Medium
 
+Tags: DP
 """
 from typing import List
 
 
 class Solution:
+    def maxProfit2(self, prices: List[int]) -> int:
+        """
+        Updated at 2021/11/10
+        Runtime: 60 ms, faster than 81.22%
+        Memory Usage: 15.1 MB, less than 24.17%
+
+        1 <= prices.length <= 3 * 10^4
+        0 <= prices[i] <= 10^4
+        :param prices:
+        :return:
+        """
+        profit = 0
+        for i in range(1, len(prices)):
+            if prices[i] > prices[i - 1]:
+                profit += prices[i] - prices[i - 1]
+        return profit
+
     def maxProfit(self, prices: List[int]) -> int:
         # Buy before price increase, sell before price decrease, you win!
         l = len(prices)
@@ -40,6 +60,12 @@ def test():
     assert Solution().maxProfit([7, 1, 5, 3, 6, 4]) == 7
     assert Solution().maxProfit([1, 2, 3, 4, 5]) == 4
     assert Solution().maxProfit([7, 6, 4, 3, 1]) == 0
+
+    assert Solution().maxProfit2([1, 2]) == 1
+    assert Solution().maxProfit2([2, 1]) == 0
+    assert Solution().maxProfit2([7, 1, 5, 3, 6, 4]) == 7
+    assert Solution().maxProfit2([1, 2, 3, 4, 5]) == 4
+    assert Solution().maxProfit2([7, 6, 4, 3, 1]) == 0
 
 
 if __name__ == '__main__':
