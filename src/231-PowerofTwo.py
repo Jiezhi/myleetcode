c76ab492d61e1185a44b6056a1adf98f4e2eab06
Jiezhi <Jiezhi@users.noreply.github.com>
2021-12-21 17:53:26 +0800

    231*2

--- src/231-PowerofTwo.py
+++ src/231-PowerofTwo.py
@@ -10,6 +10,21 @@ Difficulty: Easy
 
 
 class Solution:
+    def isPowerOfTwo2(self, n: int) -> bool:
+        """
+        Update: 2021/12/21
+        Notes: 用一条语句判断一个整数是不是2的整数次方。一个整数如果是2的整数次方，那么它的二进制表示中有且只有一位是1，而其他所有位都是0。
+        把这个整数减去1之后再和它自己做与运算，这个整数中唯一的1就会变成0。
+
+        Runtime: 24 ms, faster than 96.97%
+        Memory Usage: 14.1 MB, less than 89.51%
+        :param n: -2^31 <= n <= 2^31 - 1
+        :return:
+        """
+        if n <= 0:
+            return False
+        return n & (n - 1) == 0
+
     def isPowerOfTwo(self, n: int) -> bool:
         """
         1108 / 1108 test cases passed.
@@ -25,6 +40,10 @@ class Solution:
 
 
 def test():
+    assert Solution().isPowerOfTwo2(n=1)
+    assert Solution().isPowerOfTwo2(n=16)
+    assert not Solution().isPowerOfTwo2(n=3)
+
     assert Solution().isPowerOfTwo(n=1)
     assert Solution().isPowerOfTwo(n=16)
     assert not Solution().isPowerOfTwo(n=3)
