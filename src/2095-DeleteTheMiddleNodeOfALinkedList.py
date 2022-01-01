cd2fe41c3f987d4b10cc4c1323a508bfdf230f51
Jiezhi <Jiezhi@users.noreply.github.com>
2022-01-01 12:51:43 +0800

    2095

--- src/2095-DeleteTheMiddleNodeOfALinkedList.py
+++ src/2095-DeleteTheMiddleNodeOfALinkedList.py
@@ -3,6 +3,7 @@
 CREATED AT: 2021/12/5
 Des:
 
+https://leetcode.com/problems/delete-the-middle-node-of-a-linked-list
 https://leetcode.com/contest/weekly-contest-270/problems/delete-the-middle-node-of-a-linked-list/
 GITHUB: https://github.com/Jiezhi/myleetcode
 
