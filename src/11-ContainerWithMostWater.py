26af59fa80f4a4cb82e39edc3140a915b6092a60
Jiezhi <Jiezhi@users.noreply.github.com>
2022-01-04 17:14:54 +0800

    11

--- src/11-ContainerWithMostWater.py
+++ src/11-ContainerWithMostWater.py
@@ -1,17 +1,47 @@
 #!/usr/bin/env python
 """
+
 https://leetcode.com/problems/container-with-most-water/
+https://leetcode.com/explore/interview/card/top-interview-questions-hard/116/array-and-strings/830/
+
 Created on 2018-12-13
+Updated at 2022/01/04
 
 @author: 'Jiezhi.G@gmail.com'
 
-Reference: 
+Reference: https://leetcode.com/problems/container-with-most-water/solution/
+
+Difficulty: Medium
 """
+from typing import List
 
 
 class Solution:
-    def maxArea(self, height):
+    def maxArea(self, height: List[int]) -> int:
         """
+        Updated at 2022/01/04
+        Runtime: 1491 ms, faster than 5.01%
+        Memory Usage: 27.6 MB, less than 22.34%
+
+        n == height.length
+        2 <= n <= 10^5
+        0 <= height[i] <= 10^4
+        :param height:
+        :return:
+        """
+        low, high = 0, len(height) - 1
+        ret = 0
+        while low < high:
+            ret = max(min(height[low], height[high]) * (high - low), ret)
+            if height[low] <= height[high]:
+                low += 1
+            else:
+                high -= 1
+        return ret
+
+    def maxArea2(self, height):
+        """
+        Updated at 2018-12-13
         :type height: List[int]
         :rtype: int
         """
@@ -31,8 +61,10 @@ class Solution:
 
 
 def test():
-    assert Solution().maxArea([1, 8, 6, 2, 5, 4, 8, 3, 7]) == 49
-    assert Solution().maxArea([1, 2]) == 1
+    assert Solution().maxArea(height=list(range(1000))) == 249500
+    assert Solution().maxArea(height=[1, 2, 3, 4, 5, 4, 3, 2, 1]) == 12
+    assert Solution().maxArea(height=[1, 8, 6, 2, 5, 4, 8, 3, 7]) == 49
+    assert Solution().maxArea(height=[1, 2]) == 1
 
 
 if __name__ == '__main__':
