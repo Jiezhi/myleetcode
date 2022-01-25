dce3ebb90f3537fd5df9c6696735a2390077347b
Jiezhi <Jiezhi@users.noreply.github.com>
2022-01-25 13:37:58 +0800

    518

--- src/518-CoinChange2.py
+++ src/518-CoinChange2.py
@@ -19,6 +19,11 @@ from typing import List
 class Solution:
     def change(self, amount: int, coins: List[int]) -> int:
         """
+        Solved at 2022/01/25
+        28 / 28 test cases passed.
+        Status: Accepted
+        Runtime: 196 ms, faster than 71.16%
+        Memory Usage: 14.3 MB, less than 85.88%
         1 <= coins.length <= 300
         1 <= coins[i] <= 5000
         All the values of coins are unique.
@@ -27,6 +32,12 @@ class Solution:
         :param coins:
         :return:
         """
+        dp = [0] * (amount + 1)
+        dp[0] = 1
+        for coin in coins:
+            for i in range(coin, amount + 1):
+                dp[i] += dp[i - coin]
+        return dp[amount]
 
 
 def test():
