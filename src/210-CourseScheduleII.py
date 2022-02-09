05759944d6d2012f3c091b3af83a1fc2917160dc
Jiezhi <Jiezhi@users.noreply.github.com>
2022-02-09 10:29:27 +0800

    210

--- src/210-CourseScheduleII.py
+++ src/210-CourseScheduleII.py
@@ -4,21 +4,62 @@ CREATED AT: 2021/12/23
 Des:
 
 https://leetcode.com/problems/course-schedule-ii/
+https://leetcode.com/explore/learn/card/graph/623/kahns-algorithm-for-topological-sorting/3868/
 GITHUB: https://github.com/Jiezhi/myleetcode
 
 Difficulty: Medium
 
-Tag: 
+Tag: Graph
 
 See: 
 
 Time Spent: 30 min
 """
+import collections
 from typing import List
 
 
 class Solution:
     def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
+        """
+        CREATED AT: 2022/02/09
+        44 / 44 test cases passed.
+        Status: Accepted
+        Runtime: 204 ms, faster than 14.87%
+        Memory Usage: 15.6 MB, less than 64.15%
+        1 <= numCourses <= 2000
+        0 <= prerequisites.length <= numCourses * (numCourses - 1)
+        prerequisites[i].length == 2
+        0 <= ai, bi < numCourses
+        ai != bi
+        All the pairs [ai, bi] are distinct.
+        :param numCourses:
+        :param prerequisites:
+        :return:
+        """
+        course_dict = dict()
+        for i in range(numCourses):
+            course_dict[i] = set()
+        course_dict2 = collections.defaultdict(list)
+        for courses in prerequisites:
+            course_dict[courses[0]].add(courses[1])
+            course_dict2[courses[1]].append(courses[0])
+        ret = []
+        visited = set()
+        ret_changed = True
+        while len(visited) < numCourses and ret_changed:
+            ret_changed = False
+            for key, value in course_dict.items():
+                if not value and key not in visited:
+                    visited.add(key)
+                    ret.append(key)
+                    ret_changed = True
+                    for to_remove in course_dict2[key]:
+                        course_dict[to_remove].remove(key)
+
+        return ret if len(ret) == numCourses else []
+
+    def findOrder2(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
         """
         Runtime: 152 ms, faster than 17.37%
         Memory Usage: 19.6 MB, less than 5.80%
@@ -76,9 +117,6 @@ def test():
     assert Solution().findOrder(numCourses=1, prerequisites=[]) == [0]
     ret = [[0, 1, 2, 3], [0, 2, 1, 3]]
     assert Solution().findOrder(numCourses=4, prerequisites=[[1, 0], [2, 0], [3, 1], [3, 2]]) in ret
-
-    ret = Solution().findOrder(numCourses=6, prerequisites=[[1, 2], [3, 2], [2, 4], [5, 2], [3, 5], [0, 3]])
-    print(ret)
 
 
 if __name__ == '__main__':
