8ae75f1c0c933f99ccbe7e51de05050c22f63b59
Jiezhi <Jiezhi@users.noreply.github.com>
2022-02-14 10:45:13 +0800

    104

--- src/104-MaximumDepthOfBinaryTree.py
+++ src/104-MaximumDepthOfBinaryTree.py
@@ -11,10 +11,35 @@ Reference: https://leetcode.com/explore/learn/card/recursion-i/256/complexity-an
 Tail Recursion
 
 """
+import collections
+from typing import Optional
+
 from tree_node import *
 
 
 class Solution:
+    def maxDepth3(self, root: Optional[TreeNode]) -> int:
+        """
+        Created on 2022/2/14
+        Runtime: 40 ms, faster than 89.54%
+        Memory Usage: 15.4 MB, less than 86.50%
+        The number of nodes in the tree is in the range [0, 10^4].
+        -100 <= Node.val <= 100
+        """
+        if not root:
+            return 0
+        ret = 0
+        dq = collections.deque([(root, 1)])
+        while dq:
+            node, depth = dq.popleft()
+            if depth > ret:
+                ret = depth
+            if node.left:
+                dq.append((node.left, depth + 1))
+            if node.right:
+                dq.append((node.right, depth + 1))
+        return ret
+
     def maxDepth(self, root: TreeNode) -> int:
         if not root:
             return 0
@@ -42,6 +67,8 @@ def test():
     assert Solution().maxDepth(build_tree_node([2, None, 3, None, 4, None, 5, None, 6])) == 5
     assert Solution().maxDepth2(build_tree_node([3, 9, 20, None, None, 15, 7])) == 3
     assert Solution().maxDepth2(build_tree_node([2, None, 3, None, 4, None, 5, None, 6])) == 5
+    assert Solution().maxDepth3(build_tree_node([3, 9, 20, None, None, 15, 7])) == 3
+    assert Solution().maxDepth3(build_tree_node([2, None, 3, None, 4, None, 5, None, 6])) == 5
 
 
 if __name__ == '__main__':
