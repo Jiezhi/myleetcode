f69ffe58bc054abd3537872378293cc767e521f4
Jiezhi <Jiezhi@users.noreply.github.com>
2022-04-07 14:48:15 +0800

    119

--- src/119-PascalTriangle2.py
+++ src/119-PascalTriangle2.py
@@ -7,9 +7,28 @@ Created on 2019-07-29
 Leetcode: https://leetcode.com/problems/pascals-triangle-ii/
 
 """
+from typing import List
 
 
 class Solution:
+    def getRow2(self, rowIndex: int) -> List[int]:
+        """
+        34 / 34 test cases passed.
+        Status: Accepted
+        Runtime: 47 ms, faster than 42.34%
+        Memory Usage: 13.9 MB, less than 0
+        :param rowIndex: 0 <= rowIndex <= 33
+        :return:
+        """
+        if rowIndex == 0:
+            return [1]
+        ret = [1] * rowIndex
+        for i in range(2, rowIndex + 1):
+            pre = ret[0]
+            for j in range(1, i):
+                ret[j], pre = pre + ret[j], ret[j]
+        return ret + [1]
+
     def getNextRow(self, row: list):
         newRow = [1]
         for i in range(len(row) - 1):
@@ -28,6 +47,7 @@ def test():
     assert Solution().getRow(0) == [1]
     assert Solution().getRow(1) == [1, 1]
     assert Solution().getRow(3) == [1, 3, 3, 1]
+    assert Solution().getRow2(3) == [1, 3, 3, 1]
 
 
 if __name__ == '__main__':
