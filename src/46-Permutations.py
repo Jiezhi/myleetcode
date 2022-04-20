606d68e159dbfa07bb5301b1302e13830c941e0b
Jiezhi <Jiezhi@users.noreply.github.com>
2022-04-20 14:14:02 +0800

    46#2

--- src/46-Permutations.py
+++ src/46-Permutations.py
@@ -13,6 +13,27 @@ from typing import List
 
 
 class Solution:
+    def permute2(self, nums: List[int]) -> List[List[int]]:
+        """
+        AC: 04/20/2022 14:09
+        Runtime: 54 ms, faster than 53.41%
+        Memory Usage: 14.1 MB, less than 60.27%
+
+        1 <= nums.length <= 6
+        -10 <= nums[i] <= 10
+        All the integers of nums are unique.
+        """
+        if len(nums) <= 1:
+            return [nums]
+        num_copy = nums.copy()
+        ret = []
+        for i, num in enumerate(nums):
+            num_copy.pop(i)
+            for r in self.permute(num_copy):
+                ret.append([num] + r)
+            num_copy.insert(i, num)
+        return ret
+
     def permute(self, nums: List[int]) -> List[List[int]]:
         """
         26 / 26 test cases passed.
@@ -37,6 +58,11 @@ class Solution:
 def test():
     ans = [[1, 2, 3], [1, 3, 2], [2, 1, 3], [2, 3, 1], [3, 1, 2], [3, 2, 1]]
     ret = Solution().permute([1, 2, 3])
+    assert len(ans) == len(ret)
+    for a in ans:
+        assert a in ret
+
+    ret = Solution().permute2([1, 2, 3])
     assert len(ans) == len(ret)
     for a in ans:
         assert a in ret
