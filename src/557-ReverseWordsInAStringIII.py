6c6288bf3ba5b7900b9833accd05883e655001ec
Jiezhi <Jiezhi@users.noreply.github.com>
2022-04-20 10:34:19 +0800

    557

--- src/557-ReverseWordsInAStringIII.py
+++ src/557-ReverseWordsInAStringIII.py
@@ -1,16 +1,30 @@
 #!/usr/bin/env python
 """
-Github: https://github.com/Jiezhi/myleetcode
-
 Created on 2019/9/12
 
 Leetcode: https://leetcode.com/problems/reverse-words-in-a-string-iii/
+GITHUB: https://github.com/Jiezhi/myleetcode
+
+Difficulty: Easy
+
+Tag: 
+
+See: 
 
 """
 
 
 class Solution:
     def reverseWords(self, s: str) -> str:
+        """
+        1 <= s.length <= 5 * 10^4
+        s contains printable ASCII characters.
+        s does not contain any leading or trailing spaces.
+        There is at least one word in s.
+        All the words in s are separated by a single space.
+        :param s:
+        :return:
+        """
         return ' '.join([x[::-1] for x in s.split()])
 
 
