940bd5eeff77ffa4e30db2b85bac8d761789434a
Jiezhi <Jiezhi@users.noreply.github.com>
2022-09-14 10:26:16 +0800

    Create 1619-MeanOfArrayAfterRemovingSomeElements.py

---
+++ src/1619-MeanOfArrayAfterRemovingSomeElements.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+"""
+CREATED AT: 2022-09-14
+
+URL: https://leetcode.com/problems/mean-of-array-after-removing-some-elements/
+
+GITHUB: https://github.com/Jiezhi/myleetcode
+
+FileName: 1619-MeanOfArrayAfterRemovingSomeElements
+
+Difficulty: Easy
+
+Desc: 
+
+Tag: 
+
+See: 
+
+"""
+from tool import *
+
+
+class Solution:
+    def trimMean(self, arr: List[int]) -> float:
+        """
+        Runtime: 56 ms, faster than 98.16%
+        Memory Usage: 13.9 MB, less than 76.63%
+
+        20 <= arr.length <= 1000
+        arr.length is a multiple of 20.
+        0 <= arr[i] <= 10^5
+        """
+        arr.sort()
+        n = len(arr)
+        per5 = int(n * 0.05)
+        return sum(arr[per5:-per5]) / (n - 2 * per5)
+
+
+def test():
+    assert Solution().trimMean(arr=[1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 3]) == 2.000
+    assert Solution().trimMean(arr=[6, 2, 7, 5, 1, 2, 0, 3, 10, 2, 5, 0, 5, 5, 0, 8, 7, 6, 8, 0]) == 4.000
+    assert Solution().trimMean(
+        arr=[6, 0, 7, 0, 7, 5, 7, 8, 3, 4, 0, 7, 8, 1, 6, 8, 1, 1, 2, 4, 8, 1, 9, 5, 4, 3, 8, 5, 10, 8, 6, 6, 1, 0, 6,
+             10, 8, 2, 3, 4]) == 4.77778
+
+
+if __name__ == '__main__':
+    test()
