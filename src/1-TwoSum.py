4b7649617e9b460a69a218527774cd571c5b9e07
Jiezhi <Jiezhi@users.noreply.github.com>
2022-09-22 11:25:23 +0800

    1#3

--- src/1-TwoSum.py
+++ src/1-TwoSum.py
@@ -1,17 +1,50 @@
+#!/usr/bin/env python3
 """
- https://leetcode.com/problems/two-sum
- https://leetcode.com/explore/challenge/card/august-leetcoding-challenge-2021/613/week-1-august-1st-august-7th/3836/
- @Author: Jiezhi
- @Date: 2018-07-06 17:35:15 
- @Last Modified by:   Jiezhi
- @Last Modified time:  20210802
- """
-from typing import List
+CREATED AT: 2022-09-22
+
+URL: https://leetcode.com/problems/two-sum/
+
+GITHUB: https://github.com/Jiezhi/myleetcode
+
+FileName: 1-TwoSum
+
+Difficulty: Easy
+
+Desc: 
+
+Tag: 
+
+See: 
+
+"""
+from tool import *
 
 
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         """
+        2022-09-22
+        Runtime: 183 ms, faster than 35.85%
+        Memory Usage: 16.8 MB, less than 8.86%
+        2 <= nums.length <= 10^4
+        -10^9 <= nums[i] <= 10^9
+        -10^9 <= target <= 10^9
+        Only one valid answer exists.
+        """
+        pos_dict = defaultdict(list)
+        for pos, num in enumerate(nums):
+            pos_dict[num].append(pos)
+        if target & 1 == 0 and len(pos_dict[target // 2]) >= 2:
+            return pos_dict[target // 2][:2]
+        for num in set(nums):
+            v = target - num
+            if v != num and v in pos_dict:
+                return [pos_dict[num][0], pos_dict[v][0]]
+
+    def twoSum2(self, nums: List[int], target: int) -> List[int]:
+        """
+        2018-07-06 17:35:15
+        20210802
         54 / 54 test cases passed.
         Status: Accepted
         Runtime: 632 ms
@@ -28,6 +61,7 @@ class Solution:
 
 def test():
     assert Solution().twoSum([2, 7, 11, 15], 9) == [0, 1]
+    assert Solution().twoSum2([2, 7, 11, 15], 9) == [0, 1]
 
 
 if __name__ == '__main__':
