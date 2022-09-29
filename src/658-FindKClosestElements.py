66b02452fcd52d0257f43533c5cc2119a911d76c
Jiezhi <Jiezhi.G@gmail.com>
2022-09-29 17:10:29 +0800

    658#2

--- src/658-FindKClosestElements.py
+++ src/658-FindKClosestElements.py
@@ -16,10 +16,39 @@ See:
 Ref: https://leetcode.com/problems/find-k-closest-elements/discuss/106426/JavaC%2B%2BPython-Binary-Search-O(log(N-K)-%2B-K)
 
 """
-from typing import List
+from tool import *
 
 
 class Solution:
+    def findClosestElements2(self, arr: List[int], k: int, x: int) -> List[int]:
+        """
+        Runtime: 847 ms, faster than 12.34%
+        Memory Usage: 15.4 MB, less than 80.93%
+        1 <= k <= arr.length
+        1 <= arr.length <= 10^4
+        arr is sorted in ascending order.
+        -10^4 <= arr[i], x <= 10^4
+        """
+        i = bisect.bisect_left(arr, x) - 1
+        ret = []
+        j = i + 1
+        while len(ret) < k and i >= 0 and j < len(arr):
+            a, b = abs(arr[i] - x), abs(arr[j] - x)
+            if a == b or a < b:
+                ret.append(arr[i])
+                i -= 1
+            else:
+                ret.append(arr[j])
+                j += 1
+        if len(ret) < k:
+            while len(ret) < k and i >= 0:
+                ret.append(arr[i])
+                i -= 1
+            while len(ret) < k and j < len(arr):
+                ret.append(arr[j])
+                j += 1
+        return sorted(ret)
+
     def findClosestElements(self, arr: List[int], k: int, x: int) -> List[int]:
         """
         63 / 63 test cases passed.
@@ -30,10 +59,6 @@ class Solution:
         1 <= arr.length <= 10^4
         arr is sorted in ascending order.
         -10^4 <= arr[i], x <= 10^4
-        :param arr:
-        :param k:
-        :param x:
-        :return:
         """
         low, high = 0, len(arr) - k
         while low < high:
