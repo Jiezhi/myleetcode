1d50574f55b0ed2c46628317854067b62e885a2f
Jiezhi <Jiezhi.G@gmail.com>
2022-10-01 13:08:42 +0800

    91#2

--- src/91-DecodeWays.py
+++ src/91-DecodeWays.py
@@ -1,21 +1,30 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 """
-CREATED AT: 2021/8/18
-Des:
-https://leetcode.com/problems/decode-ways/
+CREATED AT: 2022-10-01
+
+URL: https://leetcode.com/problems/decode-ways/
 https://leetcode.com/explore/challenge/card/august-leetcoding-challenge-2021/615/week-3-august-15th-august-21st/3902/
 GITHUB: https://github.com/Jiezhi/myleetcode
 
-"""
-from functools import lru_cache
+FileName: 91-DecodeWays
 
-from tool import print_results
+Difficulty: Medium
+
+Desc: 
+
+Tag: 
+
+See: 
+
+"""
+from tool import *
 
 
 class Solution:
     @print_results
     def numDecodings(self, s: str) -> int:
         """
+        2021/8/18
         269 / 269 test cases passed.
         Status: Accepted
         Runtime: 36 ms
@@ -38,6 +47,32 @@ class Solution:
             return ans
 
         return dp(0)
+
+    def numDecodings2(self, s: str) -> int:
+        """
+        2022-10-01
+        Runtime: 37 ms, faster than 87.73%
+        Memory Usage: 14.5 MB, less than 7.07%
+
+        1 <= s.length <= 100
+        s contains only digits and may contain leading zero(s).
+        """
+        valid_two = {str(x) for x in range(10, 27)}
+
+        @cache
+        def dp(s: str) -> int:
+            if not s:
+                return 1
+            if s[0] == '0':
+                return 0
+            if len(s) == 1:
+                return 1
+            ret = dp(s[1:])
+            if s[:2] in valid_two:
+                ret += dp(s[2:])
+            return ret
+
+        return dp(s)
 
 
 def test():
