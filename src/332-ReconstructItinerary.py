0d5aa7332ef8f5dd2006f8ea4955ef774c08a5f7
Jiezhi <Jiezhi.G@gmail.com>
2022-10-19 14:35:18 +0800

    Create 332-ReconstructItinerary.py

---
+++ src/332-ReconstructItinerary.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python3
+"""
+CREATED AT: 2022-10-19
+
+URL: https://leetcode.com/problems/reconstruct-itinerary/
+
+GITHUB: https://github.com/Jiezhi/myleetcode
+
+FileName: 332-ReconstructItinerary
+
+Difficulty: Hard
+
+Desc: 
+
+Tag: 
+
+See: 
+
+"""
+from tool import *
+
+
+class Solution:
+    def findItinerary(self, tickets: List[List[str]]) -> List[str]:
+        """
+        Runtime: 998 ms, faster than 5.04%
+        Memory Usage: 35.7 MB, less than 7.12%
+
+        1 <= tickets.length <= 300
+        tickets[i].length == 2
+        fromi.length == 3
+        toi.length == 3
+        fromi and toi consist of uppercase English letters.
+        fromi != toi
+        """
+        adj = collections.defaultdict(list)
+        cnt = Counter()
+        for a, b in tickets:
+            adj[a].append(b)
+            cnt[(a, b)] += 1
+        for k in adj.keys():
+            adj[k] = sorted(adj[k], reverse=True)
+
+        stack = [(['JFK'], cnt)]
+        while stack:
+            ret, cnt = stack.pop()
+            if len(ret) == len(tickets) + 1:
+                return ret
+            k = ret[-1]
+            for v in adj[k]:
+                if cnt[(k, v)] <= 0:
+                    continue
+                retcopy = ret.copy()
+                ccopy = cnt.copy()
+                ccopy[(k, v)] -= 1
+                retcopy.append(v)
+                stack.append((retcopy, ccopy))
+
+
+def test():
+    tickets = [["MUC", "LHR"], ["JFK", "MUC"], ["SFO", "SJC"], ["LHR", "SFO"]]
+    answer = ["JFK", "MUC", "LHR", "SFO", "SJC"]
+
+    assert Solution().findItinerary(tickets=tickets) == answer
+
+
+if __name__ == '__main__':
+    test()
