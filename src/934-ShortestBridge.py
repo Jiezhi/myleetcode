2251d27278d97ebe0d6771d5b03055ed2a0d5e0a
Jiezhi <Jiezhi.G@gmail.com>
2022-10-25 14:24:16 +0800

    Update 934-ShortestBridge.py

--- src/934-ShortestBridge.py
+++ src/934-ShortestBridge.py
@@ -23,53 +23,99 @@ from tool import *
 class Solution:
     def shortestBridge(self, grid: List[List[int]]) -> int:
         """
+        https://leetcode.cn/problems/shortest-bridge/solution/zui-duan-de-qiao-by-leetcode-solution-qe44/
+        Runtime: 1081 ms, faster than 35.51%
+        Memory Usage: 17.1 MB, less than 57.08%
+
         n == grid.length == grid[i].length
         2 <= n <= 100
         grid[i][j] is either 0 or 1.
         There are exactly two islands in grid.
         """
         n = len(grid)
+        a_set = set()
+
         dirs = [(0, 1), (0, -1), (1, 0), (-1, 0)]
 
-        def bfs(x, y):
-            connect = set()
-            ret = n ** 2
-            stack = [(x, y)]
-            while stack:
-                a, b = stack.pop()
-                if not 0 <= a < n or not 0 <= b < n or (a, b) in connect or grid[a][b] == 0:
-                    continue
-                connect.add((a, b))
+        def bfs(x, y, s):
+            if 0 <= x < n and 0 <= y < n and grid[x][y] == 1 and (x, y) not in s:
+                s.add((x, y))
+                grid[x][y] = -1
                 for dx, dy in dirs:
-                    stack.append((a + dx, b + dy))
+                    bfs(x + dx, y + dy, s)
 
-
-            hq = [(1, x, y) for x, y in connect]
-            while hq:
-                d, x, y = heapq.heappop(hq)
-                if not 0 <= x < n or not 0 <= y < n:
-                    continue
-                if grid[x][y] == 1:
-                    if (x, y) not in connect:
-                        ret = min(ret, d)
-                if grid[x][y] == 0 or grid[x][y] >= d:
-                    grid[x][y] = d
-                    for dx, dy in dirs:
-                        heapq.heappush(hq, (d + 1, x + dx, y + dy))
-            return ret - 2
-
-
+        found = False
         for i in range(n):
             for j in range(n):
                 if grid[i][j] == 1:
-                    return bfs(i, j)
+                    bfs(i, j, a_set)
+                    found = True
+                    break
+            if found:
+                break
+
+        q = [(x, y, 0) for x, y in a_set]
+        while q:
+            tmp = []
+            for x, y, cnt in q:
+                for dx, dy in dirs:
+                    nx, ny = x + dx, y + dy
+                    if 0 <= nx < n and 0 <= ny < n:
+                        if grid[nx][ny] == 1:
+                            return cnt
+                        if grid[nx][ny] == 0:
+                            grid[nx][ny] = -1
+                            tmp.append((nx, ny, cnt + 1))
+            q = tmp
+
+    def shortestBridge2(self, grid: List[List[int]]) -> int:
+        """
+        AC on LCCN
+        TLE on LC
+        n == grid.length == grid[i].length
+        2 <= n <= 100
+        grid[i][j] is either 0 or 1.
+        There are exactly two islands in grid.
+        """
+        n = len(grid)
+        a_set = set()
+
+        dirs = [(0, 1), (0, -1), (1, 0), (-1, 0)]
+
+        def bfs(x, y, s):
+            if 0 <= x < n and 0 <= y < n and grid[x][y] == 1 and (x, y) not in s:
+                s.add((x, y))
+                grid[x][y] = -1
+                for dx, dy in dirs:
+                    bfs(x + dx, y + dy, s)
+
+        found = False
+        for i in range(n):
+            for j in range(n):
+                if grid[i][j] == 1:
+                    bfs(i, j, a_set)
+                    found = True
+                    break
+            if found:
+                break
+
+        dq = deque((x, y, 0) for x, y in a_set)
+        while dq:
+            x, y, cnt = dq.popleft()
+            if not 0 <= x < n or not 0 <= y < n or grid[x][y] == -1:
+                continue
+            if grid[x][y] == 1:
+                return cnt - 1
+            for dx, dy in dirs:
+                dq.append((x + dx, y + dy, cnt + 1))
+
 
 def test():
-    assert Solution().shortestBridge(grid = [[0,1],[1,0]]) == 1
-    assert Solution().shortestBridge(grid = [[0,1,0],[0,0,0],[0,0,1]]) == 2
-    assert Solution().shortestBridge(grid = [[1,1,1,1,1],[1,0,0,0,1],[1,0,1,0,1],[1,0,0,0,1],[1,1,1,1,1]]) == 1
+    assert Solution().shortestBridge(grid=[[0, 1], [1, 0]]) == 1
+    assert Solution().shortestBridge(grid=[[0, 1, 0], [0, 0, 0], [0, 0, 1]]) == 2
+    assert Solution().shortestBridge(
+        grid=[[1, 1, 1, 1, 1], [1, 0, 0, 0, 1], [1, 0, 1, 0, 1], [1, 0, 0, 0, 1], [1, 1, 1, 1, 1]]) == 1
 
 
 if __name__ == '__main__':
     test()
-
